d7341e25bba6e7a17868db02f8b7e961398963fc
MurilloMorgado <mumorgado@hotmail.com>
2025-04-29 20:52:45 -0300

    first commit

---
+++ src/main/java/morgado/com/br/smart_market/SmartMarketApplication.java
@@ -0,0 +1,13 @@
+package morgado.com.br.smart_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/morgado/com/br/smart_market/SmartMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package morgado.com.br.smart_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
