7bd3d8a3e82fac091d40eedeb0129ecf79d20948
MurilloMorgado <mumorgado@hotmail.com>
2025-05-01 17:47:34 -0300

    supermercado no domain

---
+++ src/main/java/morgado/com/br/smart_market/domain/models/Supermercado.java
@@ -0,0 +1,14 @@
+package morgado.com.br.smart_market.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Supermercado {
+  
+  private Long id;
+  private String nome;
+}
